1a09a9f1bb7e42e7e2fc6e782ed10a1d69201981
shawnfeng <shawnfeng0@gmail.com>
2021-07-04 05:32:52 +0800

    fix: windows platform path display error

--- win32/generate_version_file.cmd
+++ win32/generate_version_file.cmd
@@ -8,4 +8,4 @@ for /F %%i in ('%GIT_DESCRIBE%') do ( set VERSION=%%i)
 echo #define REPO_VERSION "%VERSION%" > .\repo_version.h
 
 echo Version information: %VERSION%
-echo Version information header file path: %~dp0repo_version.h
+echo Version information header file path: %cd%\repo_version.h
