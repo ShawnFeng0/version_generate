411249cd61e3f2030531ef11ce83459cfad28eb7
shawnfeng <shawnfeng0@gmail.com>
2021-07-04 05:33:24 +0800

    Add "#pragma once" to header file

--- linux/generate_version_file.sh
+++ linux/generate_version_file.sh
@@ -2,7 +2,8 @@ cd `dirname $0`
 GIT_DESCRIBE=`cat ../git_describe.txt`
 
 cd ../../
-echo "#define REPO_VERSION \"$($GIT_DESCRIBE)\"" > ./repo_version.h
+echo "#pragma once" > repo_version.h
+echo "#define REPO_VERSION \"$($GIT_DESCRIBE)\"" > repo_version.h
 
 echo "Version information: $($GIT_DESCRIBE)"
 echo "Version information header file path: `pwd`/repo_version.h"
--- win32/generate_version_file.cmd
+++ win32/generate_version_file.cmd
@@ -5,7 +5,8 @@ set /P GIT_DESCRIBE=<..\git_describe.txt
 
 cd ..\..\
 for /F %%i in ('%GIT_DESCRIBE%') do ( set VERSION=%%i)
-echo #define REPO_VERSION "%VERSION%" > .\repo_version.h
+echo #pragma once > .\repo_version.h
+echo #define REPO_VERSION "%VERSION%" >> .\repo_version.h
 
 echo Version information: %VERSION%
 echo Version information header file path: %cd%\repo_version.h
